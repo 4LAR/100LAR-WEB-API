7c96ae133b0e8979344d235870a98d462b7a9881
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2020-06-24 12:25:10 +0300

    0.2

--- SLW_API.py
+++ SLW_API.py
@@ -1,14 +1,20 @@
 #
-#   100LAR-WEB API
+#           100LAR-WEB API
 #
+#   [ok] Работа с чатом (отправка и чтение)
+#   [ok] Работа с почтой (отправка и чтение)
+#   [NOPE] Админские настройки
+#   [NOPE] Создание новостей
 
-ver = '0.1'
+ver = '0.2'
 def version():
     return ver
 
 import requests, os, time
 
 mysession = requests.session()
+
+#account
 def logout(addr):
     response = mysession.post(str(addr) + 'logout')
     return response.content
@@ -19,6 +25,7 @@ def login(addr, name, password):
     response = mysession.post(str(addr) + 'login', data)
     return response.content
 
+#chat
 def sendMessage(addr, id, password, message):
     response = mysession.get(str(addr) + 'sendMessage?id=' + str(id) + '&message=' + str(message) + '&pass=' + str(password))
     return response.content
@@ -30,3 +37,28 @@ def readMessages(addr, id, password):
     for t in text_arr:
         ft.append(t.split(':'))
     return ft
+
+#mail
+def getMailCount(addr):
+    response = mysession.get(str(addr) + 'getMailCount')
+    return int(response.text)
+
+def delMail(addr, num):
+    response = mysession.get(str(addr) + 'delMail?num=' + str(num))
+    return int(response.text)
+
+def delMails(addr):
+    response = mysession.get(str(addr) + 'delMails')
+    return int(response.text)
+
+def getMail(addr, num):
+    response = mysession.get(str(addr) + 'getMail?num=' + str(num))
+    if response.text != 'clear':
+        # [type, user_id, head, time, message]
+        return [(response.text).split('|')[0], (response.text).split('|')[1], (response.text).split('|')[2], (response.text).split('|')[3], (response.text).split('|')[4]]
+    else:
+        return 'clear'
+
+def sendMail(addr, r_id, head, text):
+    response = mysession.get(str(addr) + 'sendMail?r_id=' + str(r_id) + '&head=' + str(head) + '&text=' + str(text))
+    return response.content
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print('-'*35)
 print('API version: ' + web_api.version())
 print('100LAR-WEB-BOT')
 
-user = ['логин', 'пароль'] # данные нашего бота
+user = ['логин', 'пароль'] # данные вашего бота
 addr = 'http://127.0.0.1/' # здесь адресс сайта (обязательно в конце /)
 web_api.login(addr, user[0], user[1]) # входим в аккаунт
 room = ['id комнаты', 'пароль комнаты'] # данные чат комнаты
@@ -17,16 +17,28 @@ def sendMessage(message): # функция отправки сообщения
     web_api.sendMessage(addr, room[0], room[1], message)
 
 len_messages = len(web_api.readMessages(addr, room[0], room[1])) # получаем количество сообщений на момент запуска бота
+len_mails = web_api.getMailCount(addr) # получаем количество сообщений почты на момент запуска бота
 print('-'*35)
 while True:
+    # работа с почтой
+    count_mails = web_api.getMailCount(addr) # получаем количество сообщений
+    if count_mails > len_mails: # смотрим есть ли новые сообщение
+        for i in range(count_mails - len_mails): # обрабатываем каждое новое сообщение
+            m = web_api.getMail(addr, len_mails + i)
+            # m = [type, user_id, head, time, message]
+            print('MAIL [' + m[3] + '] ' + m[1] + ' [' +  m[0] + ':' + m[2] + ' ] =>' + m[4])
+            if m[4] == '/hello': # роверяем сообщение и если совпало то отправляем ответ
+                print(web_api.sendMail(addr, m[1], 'Stone_BOT', '<p>HELLO WORLD</p>')) # отправка сообщения
+        len_mails = count_mails # обновление количества сообщений
+    # работа с чатом
     messages = web_api.readMessages(addr, room[0], room[1]) # получаем все сообщения комнаты
     if len(messages) > len_messages: # смотрим есть ли новые сообщение
         messages_arr = messages[len_messages:len(messages)] # получаем список новых сообщений
         for m in messages_arr: # обрабатываем каждое новое сообщение
             if m[0] != user[0]: # чтобы бот сам себе не отвечал
-                #m = [id user, data(d.m.y), username, message]
-                print('[' + m[1] + '] ' + m[0] + ':' +  m[2] + ' =>' + m[3])
+                # m = [id user, data(d.m.y), username, message]
+                print('CHAT [' + m[1] + '] ' + m[0] + ':' +  m[2] + ' =>' + m[3])
                 if m[3] == '/hello': # проверяем сообщение и если совпало то отправляем ответ
                     sendMessage('hello world') # отправка сообщения
-        len_messages = len(messages)
+        len_messages = len(messages) # обновление количества сообщений
     time.sleep(1.5) # для стабильности ждём 1.5 секунды
